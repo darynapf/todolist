68603490e29ab5591aada0f8c342b53845d151c5
Daryna Parfeniuk <darina.parfeniuk@gmail.com>
2021-01-28 18:39:18 +0200

    folder edit

--- JS/main.js
+++ JS/main.js
@@ -8,6 +8,12 @@ let color = [
     "#D83754",
     "#0B97FD",
     "#98A499",
+    "#00B0FF",
+    "#E2B7A2",
+    "#0FC796",
+    "#D82C7B",
+    "#F4BE01",
+    "#FF7695",
   ],
   foldersList = document.querySelector(".folders__list"),
   tasksList = document.querySelector(".tasks__list"),
@@ -21,22 +27,43 @@ const newFolder = document.querySelector(".folders__new-folder"),
 //Active folder
 foldersList.addEventListener("click", function (event) {
   let currentFolder = event.target;
-  foldersItem.forEach(e => e.classList.remove("active-folder"));
+  foldersItem.forEach((e) => e.classList.remove("active-folder"));
   currentFolder.classList.add("active-folder");
   foldersItem = document.querySelectorAll(".folders__list__item");
-})
+});
 
 //Create new folder with random marker
 newFolder.addEventListener("click", function () {
-  let folderItem = document.createElement("li"),
-    marker = document.createElement("span");
+  const folderItem = document.createElement("li"),
+    marker = document.createElement("span"),
+    editIcon = document.createElement("img");
   folderItem.className = "folders__list__item";
   folderItem.textContent = "New Folder";
   folderItem.prepend(marker);
   foldersList.append(folderItem);
+  folderItem.append(editIcon);
+  editIcon.classList = "folders__list__item__edit";
+  editIcon.setAttribute("src", "img/edit.svg");
+  editIcon.setAttribute("alt", "edit");
   marker.className = "marker";
   marker.style.background = color[Math.floor(Math.random() * color.length)];
 });
+//hover action
+document
+  .querySelectorAll(".folders__list__item")
+  .forEach(e => e.addEventListener("mouseover", function () {
+    document.querySelector(".folders__list__item__edit").style.display = "inline";
+  }))
+document
+  .querySelector(".folders__list__item")
+  .addEventListener("mouseout", function () {
+    document.querySelector(".folders__list__item__edit").style.display = "none";
+  });
+
+  //Rename folder
+
+  const editInput = document.createElement("input");
+  editInput.setAttribute("type", "text");
 
 
 //MAIN
